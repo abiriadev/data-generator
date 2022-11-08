ddce1b0cad31d06cd18667a14ce36afcde63ef57
Abiria <abiria.dev@gmail.com>
2022-11-08 16:39:18 +0900

    feat: add `DateTime` for `createdAt` field

--- src/structures.rs
+++ src/structures.rs
@@ -1,15 +1,22 @@
-use fake::faker::name::raw::Name;
-use fake::locales::EN;
-use fake::Dummy;
-use fake::Fake;
+use chrono::{DateTime, Utc};
+use fake::{
+	faker::{chrono::raw::DateTime as FakeDateTime, name::raw::Name},
+	locales::EN,
+	Dummy, Fake,
+};
 use serde::Serialize;
 
-use crate::dummy::Nanoid;
+use crate::{dummy::Nanoid, utils::to_timestamp};
 
 #[derive(Serialize, Debug, Dummy)]
+#[serde(rename(serialize = "camelCase"))]
 pub struct User {
 	id: Nanoid,
 
 	#[dummy(faker = "Name(EN)")]
 	name: String,
+
+	#[dummy(faker = "FakeDateTime(EN)")]
+	#[serde(serialize_with = "to_timestamp")]
+	created_at: DateTime<Utc>,
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,18 @@
+use chrono::{DateTime, Utc};
 use fake::{vec, Dummy, Faker};
+use serde::Serializer;
 
 pub fn generate_n_times<T, const N: usize>() -> Vec<T>
-where
-	T: Dummy<Faker>,
-{
+where T: Dummy<Faker> {
 	vec![T; N]
+}
+
+pub fn to_timestamp<S>(
+	date: &DateTime<Utc>,
+	serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+	S: Serializer,
+{
+	serializer.serialize_i64(date.timestamp())
 }
